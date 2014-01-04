b8561269fa3ed213adee337130b0277c83b30ea1
Valery Tikhonov <com.tihon@mail.ru>
2014-01-04 19:36:46 +0000

    add todos

--- whirlwind/src/cipherSupport.c
+++ whirlwind/src/cipherSupport.c
@@ -14,7 +14,7 @@
  * @param maxx - максимально допустимое значениеs
  */
 unsigned long randVal(CipherInst *conf, unsigned long max)	//TODO test (max <0), псевдослучайность, max == 1
-{
+{//TODO случайное значение убогое
 	unsigned long out = 0;
 	lrand48_r(conf->support->randomBuffer, &out);
 	return max? out % max : out;
